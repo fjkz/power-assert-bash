87ed7b5cca1a669e10b05b8d8a580a70cf2002f4
Kazuho Fujii <kazuho.fujii@gmail.com>
2016-09-25 15:44:50 +0900

    Describe as failing with ! and -zndfsrwx options

--- power-assert.bash
+++ power-assert.bash
@@ -127,60 +127,113 @@ function powerassert_error_point() {
   done
 }
 
-# print descriptive messages
-function powerassert_describe() {
-  err_point=($(powerassert_error_point))
+# remove spaces and brackets from command sentence
+function powerassert_get_equation() {
+  sentence="$1"
+  echo "${sentence}"                 |
+    sed -e 's/^\[\[\[[[:space:]]*//' |
+    sed -e 's/[[:space:]]*\]\]\]$//'
+}
 
-  sentence=$(head "${err_point[0]}" -n "${err_point[1]}" |
-             tail -n 1)
-
-  echo "assertion error:"
-
-  # trim commant and spaces
-  sentence=$(echo "${sentence}" |
-    sed -e 's/#.*$//'           |
-    sed -e 's/^[[:space:]]*//'  |
-    sed -e 's/[[:space:]]*$//')
-
-  # expect sentence is one line
-  if [[ ! ${sentence} =~ ^\[\[\[.*\]\]\]$ ]]; then
-    return
-  fi
-
-  echo ""
-  echo "${sentence}  ->  false"
-
-  if [ "$#" -ne 3 ]; then
-    powerassert_expand "$@"
-    return
-  fi
-
-  # case: [[[ <left_name> <operator> <right_name> ]]]
-
-  # remove spaces and brackets
-  equation=$(echo "${sentence}"                  |
-    sed -e 's/^[[:space:]]*\[\[\[[[:space:]]*//' |
-    sed -e 's/[[:space:]]*\]\]\][[:space:]]*$//')
-
-  left_name=""
-  right_name=""
-
+# get left term from equation string
+function powerassert_get_left_term() {
+  equation="$1"
   # match to $a ${a} "$a" "${a}"
-  if [[ ${equation} =~ ^\$[A-Za-z1-9?]+ ]]     ||
-     [[ ${equation} =~ ^\$\{[A-Za-z1-9?]+\} ]] ||
-     [[ ${equation} =~ ^\"\$[A-Za-z1-9?]+\" ]] ||
-     [[ ${equation} =~ ^\"\$\{[A-Za-z1-9?]+\}\" ]]
+  if [[ ${equation} =~ \$[A-Za-z1-9?]+ ]]     ||
+     [[ ${equation} =~ \$\{[A-Za-z1-9?]+\} ]] ||
+     [[ ${equation} =~ \"\$[A-Za-z1-9?]+\" ]] ||
+     [[ ${equation} =~ \"\$\{[A-Za-z1-9?]+\}\" ]]
   then
-    left_name="${BASH_REMATCH[0]}"
+    echo "${BASH_REMATCH[0]}"
+  else
+    echo ""
   fi
+}
 
+# get right term from equation string
+function powerassert_get_right_term() {
+  equation="$1"
+  # match to $a ${a} "$a" "${a}"
   if [[ ${equation} =~ \$[A-Za-z1-9?]+$ ]]     ||
      [[ ${equation} =~ \$\{[A-Za-z1-9?]+\}$ ]] ||
      [[ ${equation} =~ \"\$[A-Za-z1-9?]+\"$ ]] ||
      [[ ${equation} =~ \"\$\{[A-Za-z1-9?]+\}\"$ ]]
   then
-    right_name="${BASH_REMATCH[0]}"
+    echo "${BASH_REMATCH[0]}"
+  else
+    echo ""
   fi
+}
+
+# describe in the case where
+#   [[[ <operator> <term> ]]]
+# or
+#   [[[ ! <operator> <term> ]]]
+function powerassert_describe_2word() {
+  sentence="$1"
+  shift
+  argv=("$@")
+  if [ "$#" -gt 2 ]; then
+    not="true"
+    operatior="$2"
+    val="$3"
+  else
+    not=""
+    operatior="$1"
+    val="$2"
+  fi
+  equation="$(powerassert_get_equation "${sentence}")"
+  name="$(powerassert_get_right_term "${equation}")"
+  case "${operatior}" in
+    -z | -n )
+      if [ -z "${name}" ]; then
+        return
+      fi
+      powerassert_single_point "${sentence}" "\"${val}\""
+      return
+      ;;
+
+    -d | -f | -s | -r | -w | -x )
+      if [ -n "${name}" ]; then
+        powerassert_single_point "${sentence}" "${val}"
+      fi
+      echo ""
+      echo "ls -l ${val}"
+      echo -n "->  "
+      ls -l "${val}"
+      return
+      ;;
+
+    * )
+      return
+      ;;
+  esac
+}
+
+# describe in the case where
+#   [[[ <left term> <operator> <right term> ]]]
+# or
+#   [[[ ! <left term> <operator> <right term> ]]]
+function powerassert_describe_3word() {
+  sentence="$1"
+  shift
+  argv=("$@")
+  if [ "$#" -gt 3 ]; then
+    not="true"
+    left_val="$2"
+    operatior="$3"
+    right_val="$4"
+  else
+    not=""
+    left_val="$1"
+    operatior="$2"
+    right_val="$3"
+  fi
+
+  equation="$(powerassert_get_equation "${sentence}")"
+
+  left_name="$(powerassert_get_left_term "${equation}")"
+  right_name="$(powerassert_get_right_term "${equation}")"
 
   num_var=0
   if [ "${left_name}" != "" ]; then
@@ -194,45 +247,43 @@ function powerassert_describe() {
     return
   fi
 
-  left_val="$1"
-  operatior="$2"
-  right_val="$3"
-
   case "${operatior}" in
-    == )
-      # if either of left or right value has two or more line, print diff
-      if [ $(echo "${left_val}" | wc -l) -gt 1 ] ||
-         [ $(echo "${right_val}" | wc -l) -gt 1 ]
-      then
-        powerassert_diff                 \
-          "${left_name}" "${right_name}" \
-          "${left_val}" "${right_val}"
-        return
-      fi
+    == | != )
+      # == or ! !=
+      if [[ ${operatior} == "==" && -z $not ]] ||
+         [[ ${operatior} == "!=" && -n $not ]]; then
 
-      if [ "${num_var}" -eq 2 ]; then
-        powerassert_table                     \
-          "${left_name}" "${right_name}"      \
-          "\"${left_val}\"" "\"${right_val}\""
-        return
-      fi
+        # if either of left or right value has two or more line, print diff
+        if [ $(echo "${left_val}" | wc -l) -gt 1 ] ||
+           [ $(echo "${right_val}" | wc -l) -gt 1 ]
+        then
+          powerassert_diff                 \
+            "${left_name}" "${right_name}" \
+            "${left_val}" "${right_val}"
+          return
+        fi
 
-      # ${num_val} -eq 1
-      if [ "${left_name}" != "" ]; then
-        val="${left_val}"
+        if [ "${num_var}" -eq 2 ]; then
+          powerassert_table                     \
+            "${left_name}" "${right_name}"      \
+            "\"${left_val}\"" "\"${right_val}\""
+          return
+        fi
+
+        # ${num_var} -eq 1
+        if [ "${left_name}" != "" ]; then
+          val="${left_val}"
+        else
+          val="${right_val}"
+        fi
+        powerassert_single_point "${sentence}" "\"${val}\""
+        return
+
+      # != or ! ==
       else
-        val="${right_val}"
+        powerassert_single_point "${sentence}" "\"${left_val}\""
+        return
       fi
-      powerassert_single_point "${sentence}" "\"${val}\""
-      return
-      ;;
-
-    != )
-      echo ""
-      echo "${left_name} == ${right_name}"
-      echo " |"
-      echo "\"${left_val}\""
-      return
       ;;
 
     -eq | -ne | -lt | -gt | -le | -ge )
@@ -254,8 +305,64 @@ function powerassert_describe() {
 
     *)
       if [ "${num_var}" -ne 0 ]; then
-        powerassert_expand "$@"
+        powerassert_expand "${argv[@]}"
       fi
+      return
+      ;;
+  esac
+}
+
+# get command sentence from script file
+function powerassert_get_sentence() {
+  err_point=($(powerassert_error_point))
+
+  sentence=$(head "${err_point[0]}" -n "${err_point[1]}" |
+             tail -n 1)
+
+  # trim commant and spaces
+  sentence=$(echo "${sentence}" |
+    sed -e 's/#.*$//'           |
+    sed -e 's/^[[:space:]]*//'  |
+    sed -e 's/[[:space:]]*$//')
+
+  # expect sentence is one line
+  if [[ ! ${sentence} =~ ^\[\[\[.*\]\]\]$ ]]; then
+    echo ""
+  else
+    echo "${sentence}"
+  fi
+}
+
+# print descriptive messages
+function powerassert_describe() {
+
+  echo "assertion error:"
+
+  sentence="$(powerassert_get_sentence)"
+  if [ -z "${sentence}" ]; then
+    return
+  fi
+
+  echo ""
+  echo "${sentence}  ->  false"
+
+  if [ "$1" == "!" ]; then
+    not=1
+  else
+    not=0
+  fi
+
+  case "$(($# - ${not}))" in
+    2)
+      powerassert_describe_2word "${sentence}" "$@"
+      return
+      ;;
+    3)
+      powerassert_describe_3word "${sentence}" "$@"
+      return
+      ;;
+    *)
+      powerassert_expand "$@"
       return
       ;;
   esac
